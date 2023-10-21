506780d7d1443d75f130976c94b25d1234bebe22
Kamil <kamilkwiecien9@gmail.com>
2023-10-21 12:18:12 +0200

    licznik>mianownik2

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ function App() {
       str += "1/" + ulamek + " + ";
     }
     let strKopia = str.substring(0, str.length - 3);
-    {getlicznik > getmianownik ? strKopia.includes("1/1") ? setAnswer(strKopia.replaceAll("1/1 +", "")) : null : alert("ułamek niewłaściwy")}
+    {getlicznik < getmianownik ? strKopia.includes("1/1") ? setAnswer(strKopia.replaceAll("1/1 +", "")) : null : alert("ułamek niewłaściwy")}
     return strKopia;
   }
   const click = () => {
