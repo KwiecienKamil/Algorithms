d2f7882bbcefe32538ddce0e174bfb7f2235b7d0
Kamil <kamilkwiecien9@gmail.com>
2023-10-21 11:58:30 +0200

    replacedddd

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ function App() {
       str += "1/" + ulamek + " + ";
     }
     let strKopia = str.substring(0, str.length - 3);
-    {strKopia.includes("1/1") ? setAnswer(strKopia.replaceAll("1/1", "")) : setAnswer(strKopia)}
+    {strKopia.includes("1/1") ? setAnswer(strKopia.replaceAll("1/1 +", "")) : null}
     return strKopia;
   }
   const click = () => {
