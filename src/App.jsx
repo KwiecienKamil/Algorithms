fbec02382791d702311cf3090fed2b6e4a3cb70c
Kamil <kamilkwiecien9@gmail.com>
2023-10-21 11:24:24 +0200

    donee

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,6 @@ function App() {
     listaMianownikow
   ) {
     if (licznik == 0) {
-      alert("Ułamka nie można przedstawić w układzie egipskim");
       return listaMianownikow;
     }
 
@@ -62,7 +61,7 @@ function App() {
           </div>
           <button onClick={click} className="p-2 bg-blue-400 rounded-lg hover:brightness-[90%] font-semibold">Podaj Rozkład Egipski </button>
         </div>
-        <h1 className="mt-8 text-center text-[30px]">Odpowiedź:{getmianownik > 0 ?<span className="ml-4 text-green-400">{answer}</span> : alert("Nie dziel przez zero!")}</h1> 
+        <h1 className="mt-8 text-center text-[30px]">Odpowiedź:{getlicznik > 0 ? alert("Ułamka nie można przedstawić w układzie egipskim") : getmianownik > 0 ?<span className="ml-4 text-green-400">{answer}</span> : alert("Nie dziel przez zero!")}</h1> 
         <UlamekFareya />
       </div>
     </>
